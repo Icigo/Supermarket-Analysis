3fe26525602239a1e1d76cb54fb735612e8e2641
Aditya Kar <97022380+Icigo@users.noreply.github.com>
2025-01-17 13:45:33 +0530

    Update Supermarket Analysis.sql

--- Supermarket Analysis.sql
+++ Supermarket Analysis.sql
@@ -186,13 +186,7 @@ CONCAT(FORMAT(Incremental_Revenue * 1.0 / 1000000, '00.00'), ' Million') AS Incr
 FROM cte2
 ORDER BY Incremental_Sold_Units DESC;
 
-
--- 10. What is the correlation between product category and promotion type effectiveness?
-
-
-
-
--- 11. Provide a list of products with a base price greater than 500 and that are featured in promo type of 'BOGOF' (Buy One Get One Free). 
+-- 10. Provide a list of products with a base price greater than 500 and that are featured in promo type of 'BOGOF' (Buy One Get One Free). 
 --     This information will help us identify high-value products that are currently being heavily discounted, which can be useful for evaluating our
 --     pricing and promotion strategies.
 
@@ -202,7 +196,7 @@ WHERE base_price > 500 AND promo_type = 'BOGOF'
 ORDER BY base_price DESC;
 
 
--- 12. Generate a report that provides an overview of the number of stores in each city. 
+-- 11. Generate a report that provides an overview of the number of stores in each city. 
 --     The results will be sorted in descending order of store counts, allowing us to identify the cities with the highest store presence.
 --     The report includes two essential fields: city and store count, which will assist in optimizing our retail operations.
 
@@ -211,7 +205,7 @@ FROM dim_stores
 GROUP BY city
 ORDER BY store_count DESC;
 
--- 13. Generate a report that displays each campaign along with the total revenue generated before and after the campaign? 
+-- 12. Generate a report that displays each campaign along with the total revenue generated before and after the campaign? 
 --     The report includes three key fields: campaign_name, totaI_revenue(before_promotion), totaI_revenue(after_promotion).
 --     This report should help in evaluating the financial impact of our promotional campaigns. (Display the values in millions)
 
@@ -232,7 +226,7 @@ FROM cte
 JOIN dim_campaigns c ON cte.campaign_id = c.campaign_id
 GROUP BY c.campaign_name;
 
--- 14. Produce a report that calculates the Incremental Sold Quantity (ISU%) for each category during the Diwali campaign. 
+-- 13. Produce a report that calculates the Incremental Sold Quantity (ISU%) for each category during the Diwali campaign. 
 --     Additionally, provide rankings for the categories based on their ISU%. The report will include three key fields: category, isu%, and rank order.
 --     This information will assist in assessing the category-wise success and impact of the Diwali campaign on incremental sales.
 
@@ -252,7 +246,7 @@ GROUP BY p.category
 SELECT category, CONCAT(FORMAT(ISU_percent, '00.00'), ' %') AS ISU_percent, rank
 FROM cte;
 
--- 15. Create a report featuring the Top 5 products, ranked by Incremental Revenue Percentage (IR%), across all campaigns.
+-- 14. Create a report featuring the Top 5 products, ranked by Incremental Revenue Percentage (IR%), across all campaigns.
 --     The report will provide essential information including product name, category, and ir%. 
 --     This analysis helps identify the most successful products in terms of incremental revenue across our campaigns, assisting in product optimization.
 
